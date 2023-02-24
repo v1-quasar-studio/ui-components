fa36529e6c5463431b26aa59b6f4ecf15cf010a4
HBO30 <diab.4869@gmail.com>
2023-02-24 20:09:09 +0200

    library

---
+++ src/pages/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page class="flex flex-center">
+    <cq-date v-model="range" v-slot="{ on }">
+      <q-btn @click="on.open()"> {{ range }} </q-btn>
+    </cq-date>
+  </q-page>
+</template>
+
+<script>
+import cqDate from "../components/cq-date/index.vue";
+export default {
+  name: "PageIndex",
+  components: {
+    "cq-date": cqDate
+  },
+  data() {
+    return {
+      range: {}
+    };
+  }
+};
+</script>
