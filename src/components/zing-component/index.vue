a02654046f962e4497fa013174bc083475c625f9
HBO30 <diab.4869@gmail.com>
2023-03-10 22:18:27 +0200

    feat : zing components [start]

---
+++ src/components/zing-component/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    zing components
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,17 @@
 <template>
   <q-page class="flex flex-center">
-   <local-new-component></local-new-component>
+   <!-- <local-new-component></local-new-component> -->
+   <local-zing-component></local-zing-component>
   </q-page>
 </template>
 
 <script>
-import newComponent from "../components/new-component/index.vue";
+// import newComponent from "../components/new-component/index.vue";
+import zingComponent from "../components/zing-component/index.vue";
 export default {
   components: {
-    "local-new-component": newComponent
+    // "local-new-component": newComponent
+    "local-zing-component": zingComponent
   },
   data() {
     return {
