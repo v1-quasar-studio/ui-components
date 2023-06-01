95ee8af199388a6e59ffe3e8792b6fc5a74562cd
moHaHa <diab.4869@gmail.com>
2023-06-01 05:30:29 +0300

    update

--- src/components/zing-component/index.vue
+++ src/components/zing-component/index.vue
@@ -1,15 +1,73 @@
 <template>
   <div>
-    zing components
+    <line-area-bar-chart
+      :chartType="chartType.value"
+      :height="height"
+      :labels="labels"
+      :series="chartSeries"
+      :xLabel="xLabel"
+      :yLabel="yLabel"
+      :showRanges="showRanges"
+      :twoYAxis="twoYAxis"
+      :scale-x-tooltip="scaleXTooltip"
+      :scale-y-short-values="scaleYShortValues"
+      :stacked="stacked"
+      :hide-zeros="hideZeros"
+      :scroll-x="scrollX"
+      :zoom-size="zoomSize"
+      :hide-legend="hideLegend"
+      :small-scale-y-values="smallScaleYValues"
+      :plot-style="plotStyle"
+      :custom-configuration="customConfiguration"
+      :minValue="minValue"
+    />
   </div>
 </template>
 
 <script>
-export default {
+import {
+  LineAreaBarChart,
+  LineAreaBarChartColors as colors
+} from "zingchart-vue-implementation";
 
-}
+export default {
+  components: {
+    LineAreaBarChart
+  },
+  props: {
+    height: { type: String, default: "300px" },
+    labels: {},
+    series: {},
+    xLabel: { type: String, default: "" },
+    yLabel: { type: String, default: "" },
+    showRanges: { type: Boolean, default: false },
+    twoYAxis: { type: Boolean },
+    scaleXTooltip: Boolean,
+    scaleYShortValues: { type: Boolean, default: true },
+    stacked: Boolean,
+    hideZeros: Boolean,
+    scrollX: Boolean,
+    zoomSize: Number,
+    hideLegend: Boolean,
+    smallScaleYValues: Boolean,
+    plotStyle: Object,
+    customConfiguration: { type: Object, default: () => ({}) },
+    minValue: {
+      type: Number
+    }
+  },
+  data() {
+    return {
+      chartType: { value: "line" },
+      twoXAxis:0
+    };
+  },
+  computed: {
+    chartSeries() {
+      return this.series.filter((s, i) => !this.hiddenItems.includes(i));
+    }
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,30 @@
 <template>
   <q-page class="flex flex-center">
-   <!-- <local-new-component></local-new-component> -->
-   <local-zing-component></local-zing-component>
+    <!-- <local-new-component></local-new-component> -->
+    <local-zing-component
+      :chartType="'area'"
+      :height="'270px'"
+      :hideLegend="true"
+      :hideZeros="false"
+      :labels="labels"
+      :scaleXTooltip="true"
+      :scaleYShortValues="true"
+      :scrollX="true"
+      :series="series"
+      :smallScaleYValues="false"
+      :stacked="false"
+      :twoYAxis="true"
+      :xLabel="''"
+      :yLabel="''"
+      :zoomSize="6"
+    ></local-zing-component>
   </q-page>
 </template>
 
 <script>
 // import newComponent from "../components/new-component/index.vue";
 import zingComponent from "../components/zing-component/index.vue";
+import { labels, series } from "./data.js";
 export default {
   components: {
     // "local-new-component": newComponent
@@ -15,6 +32,8 @@ export default {
   },
   data() {
     return {
+      labels,
+      series
     };
   }
 };
